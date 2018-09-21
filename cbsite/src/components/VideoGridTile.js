db3b5717625685af89066086b521f27ed0cb4e63
sunpeijia <1311107143@qq.com>
2018-09-21 16:38:25 +0800

    no message

---
+++ cbsite/src/components/VideoGridTile.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+
+import eye from "../resource/eye.png";
+
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+  gridtile: {
+  	backgroundColor: '#fff',
+  	height: '100%'
+  },
+  title: {
+  	color: '#666',
+  	marginLeft: '10px',
+  	marginTop: '5px',
+  	marginBottom: '5px',
+    cursor: 'default'
+  },
+  line: {
+    width: '90%',
+    height: '1px',
+    background: '#E0E0E0',
+    marginBottom: '5px',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
+  content: {
+  	color: '#666',
+  	marginTop: '10px',
+  	marginLeft: '10px',
+  	marginRight: '10px'
+  },
+  eyetext: {
+  	color: '#999',
+  	fontSize: '10px'
+  },
+  eyeimg: {
+  	marginTop: '16px'
+  }
+})
+
+class VideoGridTile extends Component {
+    constructor() {
+        super()
+        this.state = {
+          hover: false
+        }
+    }
+
+  toggleHover(){
+    this.setState({hover: !this.state.hover})
+  }
+
+  render() {
+    const {classes} = this.props;
+    var linkStyle;
+    if (this.state.hover) {
+      linkStyle = {color: '#3fc2ff'}
+    } else {
+      linkStyle = {color: '#666'}
+    }
+    return (
+      <div className={classes.gridtile}>
+        <div>
+          <img width='100%' height={100} src="https://material-ui.com/static/images/grid-list/breakfast.jpg" alt="sdfsad" />
+        </div>
+        <Grid container direction="row" justify="center" alignItems="flex-start">
+            <Grid item xs={9}>
+                <p className={classes.title} style={linkStyle} align="left" onMouseEnter={this.toggleHover.bind(this)} onMouseLeave={this.toggleHover.bind(this)}> 教程1 </p>
+            </Grid>
+            <Grid item xs={1}>
+                <img className={classes.eyeimg} src={eye} alt="" />
+            </Grid>
+            <Grid item xs={2}>
+              <p className={classes.eyetext}>1111</p>
+            </Grid>
+        </Grid>
+        <div className={classes.line}></div>
+        <p className={classes.content} align="left"> 描述描述描述描述描述描述，描述描述描述描述描述描述，描述描述，描述描述描述描述 </p>
+      </div>
+    );
+  }
+}
+
+VideoGridTile.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(VideoGridTile);
--- cbsite/src/pages/VideoListPage.js
+++ cbsite/src/pages/VideoListPage.js
@@ -3,6 +3,7 @@ import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 
 import ListTabBar from '../components/ListTabBar'
+import VideoGridTile from '../components/VideoGridTile'
 import eye from "../resource/eye.png";
 
 import GridList from '@material-ui/core/GridList';
@@ -22,39 +23,12 @@ const styles = theme => ({
     right: 0,
     top: 119
   },
-  line: {
-  	width: '90%',
-  	height: '1px',
-  	background: '#E0E0E0',
-  	marginBottom: '5px',
-  	marginLeft: 'auto',
-  	marginRight: 'auto'
-  },
   gridtile: {
   	backgroundColor: '#fff',
   	height: '100%'
   },
   bkdiv: {
   	backgroundColor: '#e1e1e1'
-  },
-  title: {
-  	color: '#666',
-  	marginLeft: '10px',
-  	marginTop: '5px',
-  	marginBottom: '5px'
-  },
-  content: {
-  	color: '#666',
-  	marginTop: '10px',
-  	marginLeft: '10px',
-  	marginRight: '10px'
-  },
-  eyetext: {
-  	color: '#999',
-  	fontSize: '10px'
-  },
-  eyeimg: {
-  	marginTop: '16px'
   },
   bar: {
     position: 'absolute',
@@ -69,40 +43,36 @@ class VideoListPage extends Component {
     constructor() {
         super()
         this.state = {
-          checked: true
+          checked: true,
+          hover: false
         }
     }
+
+  toggleHover(){
+    this.setState({hover: !this.state.hover})
+  }
 
   render() {
     const {classes} = this.props;
     const { checked } = this.state;
+    var linkStyle;
+    if (this.state.hover) {
+      linkStyle = {color: '#3fc2ff'}
+    } else {
+      linkStyle = {color: '#666'}
+    }
     return (
       <div className={classes.bkdiv}>
 
       	<GridList className={classes.gridList} cellHeight={250} cols={4} spacing={30}>   		
     			{listdata.map((tile,reactid) => ( 
     				<GridListTile key={tile}>
-            <Fade in={checked}
-            {...(checked ? { timeout: 500 } : {})} style={{ transitionDelay: checked ? 200*reactid : 0 }}>
-    					<div className={classes.gridtile}>
-    						<div>
-    							<img width='100%' height={100} src="https://material-ui.com/static/images/grid-list/breakfast.jpg" alt="sdfsad" />
-    						</div>
-    						<Grid container direction="row" justify="center" alignItems="flex-start">
-    	  		        <Grid item xs={9}>
-    	  		          	<p className={classes.title} align="left"> 教程1 </p>
-    	  		        </Grid>
-    	  		        <Grid item xs={1}>
-    	  		          	<img className={classes.eyeimg} src={eye} alt="" />
-    	  		        </Grid>
-    	  		        <Grid item xs={2}>
-    	  	      			<p className={classes.eyetext}>1111</p>
-    	  		        </Grid>
-    	  		    </Grid>
-    						<div className={classes.line}></div>
-    						<p className={classes.content} align="left"> 描述描述描述描述描述描述，描述描述描述描述描述描述，描述描述，描述描述描述描述 </p>
-    					</div>
-            </Fade>
+              <Fade in={checked}
+              {...(checked ? { timeout: 500 } : {})} style={{ transitionDelay: checked ? 200*reactid : 0 }}>
+                <div>
+                  <VideoGridTile />
+                </div>
+              </Fade>
     				</GridListTile>
 	        ))}
         </GridList>
